bcb046d959904dc033e29169be557ac65e507a42
YASHCHETHAN <cgowdahs697@gmail.com>
2025-07-26 17:07:25 +0530

    initalCommit

---
+++ src/main/java/com/example/yashchethan/YashchethanApplication.java
@@ -0,0 +1,13 @@
+package com.example.yashchethan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YashchethanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YashchethanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yashchethan/YashchethanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yashchethan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YashchethanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
