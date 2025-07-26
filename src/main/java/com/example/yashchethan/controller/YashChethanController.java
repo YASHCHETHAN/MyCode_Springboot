4200d17a5a7e1c32283bba3a891958d1534429df
YASHCHETHAN <cgowdahs697@gmail.com>
2025-07-27 00:05:34 +0530

    added the controller to check apache tomcat

---
+++ src/main/java/com/example/yashchethan/controller/YashChethanController.java
@@ -0,0 +1,14 @@
+package com.example.yashchethan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class YashChethanController {
+
+	@GetMapping(value = "/checkHealth")
+	public String getResponse() {
+		return "Hello Good Day Application UP and Running";
+	}
+	
+}
